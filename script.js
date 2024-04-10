91a999f2df13c116ce2c686290413b73aff8c7fb
Greta <greta.maria.brugnatti.2007@calvino.edu.it>
2024-04-10 07:20:04 +0200

    finito

---
+++ script.js
@@ -0,0 +1,23 @@
+function switchText(txt){
+    if(txt==0){
+        document.getElementById("title").innerHTML= "la luce blu emanata dallo schermo del cellulare è responsabile della minore produzione di melatonina, l'ormone del sonno: la luce artificiale invia al corpo il segnale che non è notte ma pieno giorno, il che causa difficoltà ad addormentarsi. Questo influisce notevolmente sulla memoria";
+    }
+    if(txt===1){
+        document.getElementById("title").innerHTML= "Avversione indiscriminata nei confronti degli stranieri e di tutto ciò che proviene dall'estero.";
+    }
+    if(txt===2){
+        document.getElementById("title").innerHTML= "La lettura contribuisce a migliorare l'autostima poiché arricchisce il vocabolario, potenziando le abilità linguistiche e comunicative. Fa scoprire nuove cose, ampliare i propri orizzonti, fa sentire più sicuri di sé e delle proprie capacità, ma rende anche più curiosi e aperti al mondo.";
+    }
+    if(txt===3){
+        document.getElementById("title").innerHTML= "La memoria riguarda tutto ciò che ricordi, così come la tua capacità di ricordare.";
+    }
+    if(txt===4){
+        document.getElementById("title").innerHTML= "Con l'avanzare della giornata scolastica, è dimostrato che le pause cerebrali diventano sempre più importanti. Uno studio del 2016 ha rilevato che il nostro cervello inizia a sentire la stanchezza verso la fine della giornata, portando a un notevole calo delle prestazioni nei test. Questa ricerca ha scoperto che per ogni ora che passa durante giornata, le prestazioni nei test diminuiscono in modo significativo.";
+    }
+    if(txt===5){
+        document.getElementById("title").innerHTML= "Oggi questa è riconosciuta come una vera sindrome che comporta perdita di memoria e difficoltà a concentrarsi. Un sondaggio condotto da Harvard Business Review ha rivelato che il 75% dei lavoratori afferma che gli smartphone e altri dispositivi digitali sono la principale fonte di distrazione sul luogo di lavoro";
+    }
+    if(txt===6){
+        document.getElementById("title").innerHTML= "Con l’avvento dei social network, la percentuale di persone che leggono contenuti e che li condividono o li commentano è aumentata incredibilmente, ma purtroppo questa partecipazione non è andata di pari passo con un miglioramento delle capacità di comprensione del testo, nel senso di interpretarlo, di saper leggere fra le righe elaborando delle conclusioni proprie.";
+    }
+}
